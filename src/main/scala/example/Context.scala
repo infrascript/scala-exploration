55c2a3fba4ba1ab9f81f6dd983166f8e8e7a6c85
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-05-14 21:19:00 +0100

    Move withNamespace wrapper into Context object

--- src/main/scala/example/Context.scala
+++ src/main/scala/example/Context.scala
@@ -1,7 +1,16 @@
 package example
 
+trait InContext {
+  def apply[T](fn: => Context => T): T
+}
+
 case class Context(namespace: Option[String] = None) {
-  def withNamespace(namespace: String) = { this.copy(namespace = Some(namespace)) }
+  def withNamespace(namespace: String) = {
+    val nsCtx = this.copy(namespace = Some(namespace))
+    new InContext {
+      def apply[T](inContext: => Context => T) = { inContext(nsCtx) }
+    }
+  }
 
   def registerProvider(provider: Provider): Unit = { println(s"Registering provider with context: $provider") }
 }
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -13,17 +13,20 @@ object Hello extends App {
   val c1: AwsAcmCertificate = AwsAcmCertificate("name", domainName = "thing", privateKey = c2.domainName)
   val c2: AwsAcmCertificate = AwsAcmCertificate("name", domainName = "thing2", privateKey = c1.domainName)
 
-  val cert          = AwsAcmCertificate("name", domainName = "www.example.com")
-  val multipleCerts = MultipleCertsComponent("comp", domain = "example.com")
+  val component = MultipleCertsComponent("comp", domain = "example.com")
 
-  val certs = Namespace("x") { implicit ctx =>
+  val certs = ctx.withNamespace("x") { implicit ctx =>
     List(
-      AwsAcmCertificate("name", domainName = "module.example.com"),
-      AwsAcmCertificate("name", domainName = "module.example.com"),
       AwsAcmCertificate("name", domainName = "module.example.com"),
       AwsAcmCertificate("name", domainName = "module.example.com"),
     )
   }
 
-  Runtime.render(certs ::: cert :: c1 :: c2 :: multipleCerts.resources ::: Nil)
+  val inXYZNamespace = ctx.withNamespace("xyz")
+  val otherCerts = List(
+    inXYZNamespace { implicit ctx => AwsAcmCertificate("name", domainName = "xyz.example.com") },
+    inXYZNamespace { implicit ctx => AwsAcmCertificate("name", domainName = "xyz.example.com") },
+  )
+
+  Runtime.render(List.concat(certs, otherCerts, component.resources, List(c1, c2)))
 }
